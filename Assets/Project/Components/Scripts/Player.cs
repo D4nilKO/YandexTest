3a48a791d6f7eeb1758a4c708dbe753b1a2a2c16
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2023-09-04 20:31:33 +0300

    Moving

--- Assets/Project/Components/Scripts/Player.cs
+++ Assets/Project/Components/Scripts/Player.cs
@@ -1,38 +1,9 @@
-﻿using System;
 using UnityEngine;
 
 namespace Project.Components.Scripts
 {
     public class Player : MonoBehaviour
     {
-        private Rigidbody2D _rigidbody;
-
-        [SerializeField] private float horizontalVelocity = 3f;
-        [SerializeField] private float verticalVelocity = 3f;
-
-        [SerializeField] private float divider = 0.001f;
-
-        private void Start()
-        {
-            _rigidbody = GetComponent<Rigidbody2D>();
-        }
-
-        private void Update()
-        {
-            Move();
-        }
-
-        private void Move()
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                var velocity = _rigidbody.velocity;
-                
-                velocity += Vector2.up * (verticalVelocity * divider);
-                velocity.x = horizontalVelocity;
-                
-                _rigidbody.velocity = velocity;
-            }
-        }
+    
     }
 }
---
+++ Assets/Project/Components/Scripts/PlayerMover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Project.Components.Scripts
+{
+    [RequireComponent(typeof(Player))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMover : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _startPosition;
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _verticalForce = 1.5f;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Start()
+        {
+            transform.position = _startPosition;
+
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.velocity = Vector2.zero;
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _rigidbody.AddForce(Vector2.up * _verticalForce, ForceMode2D.Force);
+                _rigidbody.velocity = new Vector2(_speed, _rigidbody.velocity.y);
+            }
+        }
+    }
+}
